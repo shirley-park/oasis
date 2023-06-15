cf6f495365892ef0f6e398fa6abe7c949edd391f
Shirley Park <shirley.park7@gmail.com>
2023-06-15 12:36:11 +1200

    add 'use client' to Timer

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect, FormEvent } from 'react'
 import Countdown, { zeroPad } from 'react-countdown'
 import { useSessionHistory } from '../hooks/useSessionHistory'
